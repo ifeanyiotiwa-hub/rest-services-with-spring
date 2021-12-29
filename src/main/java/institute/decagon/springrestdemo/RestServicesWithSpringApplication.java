e65498c352b6a199b03f30ddbd72be5d5a988cc3
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2021-12-29 18:33:48 +0100

    Contains maven config files and project structures

---
+++ src/main/java/institute/decagon/springrestdemo/RestServicesWithSpringApplication.java
@@ -0,0 +1,13 @@
+package institute.decagon.springrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServicesWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/institute/decagon/springrestdemo/RestServicesWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package institute.decagon.springrestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicesWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
